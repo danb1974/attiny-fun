89662776ec6187fbaaee8153aa41be17240d2f41
Dan Borlovan <danb1974@gmail.com>
2023-02-26 15:11:24 +0200

    Only refresh digits that have changed

--- attiny85-sh1106/src/main.cpp
+++ attiny85-sh1106/src/main.cpp
@@ -13,42 +13,45 @@
 
 //-----------------------------------------------------------------------------
 
-void blink(unsigned long msec) {
+void blink(unsigned long msec)
+{
   digitalWrite(PIN1, HIGH);
   delay(msec);
   digitalWrite(PIN1, LOW);
   delay(msec);
 }
 
-void slowBlink(void) {
+void slowBlink(void)
+{
   blink(1000);
 }
 
-void fastBlink(void) {
+void fastBlink(void)
+{
   blink(100);
 }
 
 //-----------------------------------------------------------------------------
 
-int const address = 0x3c;
-int const commands = 0x00;
-int const onecommand = 0x80;
-int const data = 0x40;
-int const onedata = 0xC0;
+#define SH1106_ADDRESS 0x3c
+#define SH1106_COMMAND 0x80
+#define SH1106_DATA 0xC0
+#define SH1106_COMMANDS 0x00
+#define SH1106_DATAS 0x40
 
-int xpos;
-int ypos;
+int SH1106_xpos;
+int SH1106_ypos;
 
 void Single(uint8_t data)
 {
-  Wire.write(onecommand);
+  Wire.write(SH1106_COMMAND);
   Wire.write(data);
 }
 
 void InitDisplay()
 {
-  Wire.beginTransmission(address);
-  Wire.write(commands);
+  Wire.beginTransmission(SH1106_ADDRESS);
+  Wire.write(SH1106_COMMANDS);
   Wire.write(0xA1); // Flip horizontal
   Wire.write(0xAF); // Display on
   Wire.endTransmission();
@@ -58,64 +61,70 @@ void ClearDisplay()
 {
   for (int p = 0; p < 8; p++)
   {
-    Wire.beginTransmission(address);
+    Wire.beginTransmission(SH1106_ADDRESS);
     Single(0xB0 + p);
     Wire.endTransmission();
 
     for (int q = 0; q < 8; q++)
     {
-      Wire.beginTransmission(address);
-      Wire.write(data);
+      Wire.beginTransmission(SH1106_ADDRESS);
+      Wire.write(SH1106_DATAS);
+
       for (int i = 0; i < 20; i++)
-      {
         Wire.write(0);
-      }
+
       Wire.endTransmission();
     }
   }
 }
 
-void PlotPoint(int x, int y)
+void PlotPoint(int x, int y, bool clear = false)
 {
-  Wire.beginTransmission(address);
-  Single(0x00 + ((x + 2) & 0x0F)); // Column low nibble
-  Single(0x10 + ((x + 2) >> 4));   // Column high nibble
-  Single(0xB0 + (y >> 3));         // Page
-  Single(0xE0);                    // Read modify write
-  Wire.write(onedata);
+  Wire.beginTransmission(SH1106_ADDRESS);
+  Single(0x00 + ((x + 2) & 0x0F));  // Column low nibble
+  Single(0x10 + ((x + 2) >> 4));    // Column high nibble
+  Single(0xB0 + (y >> 3));          // Page
+  Single(0xE0);                     // Enter read modify write
+  Wire.write(SH1106_DATA);
   Wire.endTransmission();
-  Wire.requestFrom(address, 2);
-  Wire.read();                     // Dummy read
+
+  Wire.requestFrom(SH1106_ADDRESS, 2);
+  Wire.read();                      // Dummy read
   int j = Wire.read();
-  Wire.beginTransmission(address);
-  Wire.write(onedata);
-  Wire.write((1 << (y & 0x07)) | j);
-  Single(0xEE);                    // Cancel read modify write
+
+  Wire.beginTransmission(SH1106_ADDRESS);
+  Wire.write(SH1106_DATA);
+  uint8_t mask = 1 << (y & 0x07);
+  if (clear)
+    Wire.write(~mask & j);
+  else
+    Wire.write(mask | j);
+  Single(0xEE);                     // Cancel read modify write
   Wire.endTransmission();
 }
 
 void MoveTo(int x, int y)
 {
-  xpos = x;
-  ypos = y;
+  SH1106_xpos = x;
+  SH1106_ypos = y;
 }
 
-void DrawTo(int x, int y)
+void DrawTo(int x, int y, bool clear = false)
 {
   int sx, sy, e2, err;
 
-  int dx = abs(x - xpos);
-  int dy = abs(y - ypos);
+  int dx = abs(x - SH1106_xpos);
+  int dy = abs(y - SH1106_ypos);
 
-  sx = (xpos < x) ? 1 : -1;
-  sy = (ypos < y) ? 1 : -1;
+  sx = (SH1106_xpos < x) ? 1 : -1;
+  sy = (SH1106_ypos < y) ? 1 : -1;
 
   err = dx - dy;
   for (;;)
   {
-    PlotPoint(xpos, ypos);
+    PlotPoint(SH1106_xpos, SH1106_ypos, clear);
 
-    if (xpos == x && ypos == y)
+    if (SH1106_xpos == x && SH1106_ypos == y)
       return;
 
     e2 = err << 1;
@@ -123,36 +132,36 @@ void DrawTo(int x, int y)
     if (e2 > -dy)
     {
       err = err - dy;
-      xpos = xpos + sx;
+      SH1106_xpos = SH1106_xpos + sx;
     }
     else if (e2 < dx)
     {
       err = err + dx;
-      ypos = ypos + sy;
+      SH1106_ypos = SH1106_ypos + sy;
     }
   }
 }
 
 //-----------------------------------------------------------------------------
 
-void DrawVerticalSegment(uint8_t x, uint8_t y)
+void DrawVerticalSegment(uint8_t x, uint8_t y, bool clear = false)
 {
-  MoveTo(x + 0, y + 2); DrawTo(x + 0, y + 11);
-  MoveTo(x + 1, y + 1); DrawTo(x + 1, y + 12);
-  MoveTo(x + 2, y + 0); DrawTo(x + 2, y + 13);
-  MoveTo(x + 3, y + 0); DrawTo(x + 3, y + 13);
-  MoveTo(x + 4, y + 1); DrawTo(x + 4, y + 12);
-  MoveTo(x + 5, y + 2); DrawTo(x + 5, y + 11);
+  MoveTo(x + 0, y + 2); DrawTo(x + 0, y + 11, clear);
+  MoveTo(x + 1, y + 1); DrawTo(x + 1, y + 12, clear);
+  MoveTo(x + 2, y + 0); DrawTo(x + 2, y + 13, clear);
+  MoveTo(x + 3, y + 0); DrawTo(x + 3, y + 13, clear);
+  MoveTo(x + 4, y + 1); DrawTo(x + 4, y + 12, clear);
+  MoveTo(x + 5, y + 2); DrawTo(x + 5, y + 11, clear);
 }
 
-void DrawHorizontalSegment(uint8_t x, uint8_t y)
+void DrawHorizontalSegment(uint8_t x, uint8_t y, bool clear = false)
 {
-  MoveTo(x + 2, y + 0); DrawTo(x + 11, y + 0);
-  MoveTo(x + 1, y + 1); DrawTo(x + 12, y + 1);
-  MoveTo(x + 0, y + 2); DrawTo(x + 13, y + 2);
-  MoveTo(x + 0, y + 3); DrawTo(x + 13, y + 3);
-  MoveTo(x + 1, y + 4); DrawTo(x + 12, y + 4);
-  MoveTo(x + 2, y + 5); DrawTo(x + 11, y + 5);
+  MoveTo(x + 2, y + 0); DrawTo(x + 11, y + 0, clear);
+  MoveTo(x + 1, y + 1); DrawTo(x + 12, y + 1, clear);
+  MoveTo(x + 0, y + 2); DrawTo(x + 13, y + 2, clear);
+  MoveTo(x + 0, y + 3); DrawTo(x + 13, y + 3, clear);
+  MoveTo(x + 1, y + 4); DrawTo(x + 12, y + 4, clear);
+  MoveTo(x + 2, y + 5); DrawTo(x + 11, y + 5, clear);
 }
 
 // orientation, x, y
@@ -166,7 +175,7 @@ const uint8_t PROGMEM segment_offsets[7][3] = {
   { 0, 4,  16 }
 };
 
-void DrawSegment(uint8_t x, uint8_t y, uint8_t segment)
+void DrawSegment(uint8_t x, uint8_t y, uint8_t segment, bool clear= false)
 {
   if (segment >= 'a' && segment <= 'g')
    segment -= 'a';
@@ -179,9 +188,9 @@ void DrawSegment(uint8_t x, uint8_t y, uint8_t segment)
   uint8_t oy = pgm_read_byte(&segment_offsets[segment][2]);
 
   if (o == 0)
-    DrawHorizontalSegment(x + ox, y + oy);
+    DrawHorizontalSegment(x + ox, y + oy, clear);
   else
-    DrawVerticalSegment(x + ox, y + oy);
+    DrawVerticalSegment(x + ox, y + oy, clear);
 }
 
 const uint8_t PROGMEM digit_segments[10][8] = {
@@ -197,7 +206,7 @@ const uint8_t PROGMEM digit_segments[10][8] = {
   "abcdfg"
 };
 
-void DrawDigit(uint8_t x, uint8_t y, uint8_t digit)
+void DrawDigit(uint8_t x, uint8_t y, uint8_t digit, bool clear = false)
 {
   if (digit >= '0' && digit <= '9')
     digit -= '0';
@@ -205,36 +214,59 @@ void DrawDigit(uint8_t x, uint8_t y, uint8_t digit)
   if (digit > 9)
     return;
 
-  for (uint8_t i = 0; i < 8; i++) {
+  for (uint8_t i = 0; i < 8; i++)
+  {
     uint8_t c = pgm_read_byte(&digit_segments[digit][i]);
     if (c == 0)
       break;
 
-    DrawSegment(x, y, c);
+    DrawSegment(x, y, c, clear);
   }
 }
 
-void DrawDot(uint8_t x, uint8_t y, uint8_t size)
+void DrawDot(uint8_t x, uint8_t y, uint8_t size, bool clear = false)
 {
-  for (uint8_t i = 0; i < size; i++) {
+  for (uint8_t i = 0; i < size; i++)
+  {
     MoveTo(x, y + i);
-    DrawTo(x + size - 1, y + i);
+    DrawTo(x + size - 1, y + i, clear);
   }
 }
 
-void DrawDots()
+void DrawDots(bool clear = false)
 {
-  DrawDot(61, 13, 6);
-  DrawDot(61, 24, 6);
+  // DrawDot(61, 13, 6, clear);
+  // DrawDot(61, 24, 6, clear);
+  DrawDot(61, 5 + 14, 4, clear);
+  DrawDot(61, 5 + 25, 4, clear);
 }
 
-void DrawDigits(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_t digit4)
+void DrawDigitPos(uint8_t pos, uint8_t digit, bool clear = false)
 {
-  DrawDigit(8, 5, digit1);
-  DrawDigit(34, 5, digit2);
-  DrawDigit(64 + 8, 5, digit3);
-  DrawDigit(64 + 34, 5, digit4);
+  switch (pos)
+  {
+    case 0:
+      DrawDigit(8, 5, digit, clear);
+      break;
+    case 1:
+      DrawDigit(34, 5, digit, clear);
+      break;
+    case 2:
+      DrawDigit(64 + 8, 5, digit, clear);
+      break;
+    case 3:
+      DrawDigit(64 + 34, 5, digit, clear);
+      break;
+  }
 }
+
+// void DrawDigits(uint8_t digit1, uint8_t digit2, uint8_t digit3, uint8_t digit4)
+// {
+//   DrawDigitPos(0, digit1);
+//   DrawDigitPos(1, digit2);
+//   DrawDigitPos(2, digit3);
+//   DrawDigitPos(3, digit4);
+// }
 
 //-----------------------------------------------------------------------------
 
@@ -253,14 +285,16 @@ protected:
   uint8_t y, m, d, hh, mm, ss;
 };
 
-static uint8_t conv2d(const char *p) {
+static uint8_t conv2d(const char *p)
+{
   uint8_t v = 0;
   if ('0' <= *p && *p <= '9')
     v = *p - '0';
   return 10 * v + *++p - '0';
 }
 
-DateTime::DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t min, uint8_t sec) {
+DateTime::DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t min, uint8_t sec)
+{
   y = year;
   m = month;
   d = day;
@@ -269,36 +303,38 @@ DateTime::DateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint
   ss = sec;
 }
 
-DateTime::DateTime(const char *date, const char *time) {
+DateTime::DateTime(const char *date, const char *time)
+{
   // sample input: date = "Dec 26 2009", time = "12:34:56"
   y = conv2d(date + 7) * 100 + conv2d(date + 9);
   // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
   m = 1;
-  switch (date[0]) {
-    case 'J':
-      m = date[1] == 'a' ? 1 : (m = date[2] == 'n' ? 6 : 7);
-      break;
-    case 'F':
-      m = 2;
-      break;
-    case 'A':
-      m = date[2] == 'r' ? 4 : 8;
-      break;
-    case 'M':
-      m = date[2] == 'r' ? 3 : 5;
-      break;
-    case 'S':
-      m = 9;
-      break;
-    case 'O':
-      m = 10;
-      break;
-    case 'N':
-      m = 11;
-      break;
-    case 'D':
-      m = 12;
-      break;
+  switch (date[0])
+  {
+  case 'J':
+    m = date[1] == 'a' ? 1 : (m = date[2] == 'n' ? 6 : 7);
+    break;
+  case 'F':
+    m = 2;
+    break;
+  case 'A':
+    m = date[2] == 'r' ? 4 : 8;
+    break;
+  case 'M':
+    m = date[2] == 'r' ? 3 : 5;
+    break;
+  case 'S':
+    m = 9;
+    break;
+  case 'O':
+    m = 10;
+    break;
+  case 'N':
+    m = 11;
+    break;
+  case 'D':
+    m = 12;
+    break;
   }
   d = conv2d(date + 4);
 
@@ -323,7 +359,8 @@ uint8_t RtcIsRunning()
   return !(ss >> 7);
 }
 
-void RtcAdjust(const DateTime &dt) {
+void RtcAdjust(const DateTime &dt)
+{
   Wire.beginTransmission(DS1307_ADDRESS);
   Wire.write(0);
   Wire.write(bin2bcd(dt.second()));
@@ -337,7 +374,8 @@ void RtcAdjust(const DateTime &dt) {
   Wire.endTransmission();
 }
 
-DateTime RtcNow() {
+DateTime RtcNow()
+{
   Wire.beginTransmission(DS1307_ADDRESS);
   Wire.write(0);
   Wire.endTransmission();
@@ -377,8 +415,9 @@ void setup()
 
 //-----------------------------------------------------------------------------
 
-uint8_t oldDigits[4];
+uint8_t oldDigits[4] = {0xff, 0xff, 0xff, 0xff};
 uint8_t digits[4];
+bool dots = true;
 
 void loop()
 {
@@ -388,13 +427,20 @@ void loop()
   digits[2] = dt.minute() / 10;
   digits[3] = dt.minute() % 10;
 
-  if (memcmp(digits, oldDigits, 4) != 0) {
-    memcpy(oldDigits, digits, 4);
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    if (digits[i] != oldDigits[i])
+    {
+      if (oldDigits[i] != 0xff)
+        DrawDigitPos(i, oldDigits[i], true);
 
-    ClearDisplay();
-    DrawDots();
-    DrawDigits(digits[0], digits[1], digits[2], digits[3]);
+      DrawDigitPos(i, digits[i], false);
+      oldDigits[i] = digits[i];
+    }
   }
+
+  DrawDots(dots);
+  dots = !dots;
 
   delay(1000);
 }
