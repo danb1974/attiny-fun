f582afca364b87162c994a87e613644e5c21fe96
Dan Borlovan <danb1974@users.noreply.github.com>
2024-10-06 17:34:34 +0300

    Very fancy version

--- attiny85-blinky/src/main.cpp
+++ attiny85-blinky/src/main.cpp
@@ -1,12 +1,42 @@
 #include <Arduino.h>
 
+// Very fancy version :)
+
+// PB5 PCINT5 | ADC0 | RESET
+// PB4 PCINT4 | ADC2 | USB- | PWM4
+// PB3 PCINT3 | ADC3 | USB+
+// PB2 PCINT2 | ADC1 | SCLK | SCL | INT0
+// PB1 PCINT1 | PWM1 | MISO | LED
+// PB0 PCINT0 | PWM0 | MOSI | SDA
+
+#define PIN_LED PIN1      // some versions have the led on PIN0
+#define BLINK_PERIOD 500  // ms between state changes
+
+//
+
+static uint8_t ledState = LOW;
+
+void initLed() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void updateLedState() {
+  digitalWrite(PIN_LED, ledState);
+}
+
+void toggleLedState() {
+  ledState = ledState == LOW ? HIGH : LOW;
+}
+
+//
+
 void setup() {
-  pinMode(1, OUTPUT);
+  initLed();
 }
 
 void loop() {
-  digitalWrite(1, HIGH);
-  delay(1000);
-  digitalWrite(1, LOW);
-  delay(1000);
+  updateLedState();
+  toggleLedState();
+
+  delay(BLINK_PERIOD);
 }
