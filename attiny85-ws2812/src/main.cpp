7f850ec111c19244cf7f4b3981960a9c9ad458db
Dan Borlovan <danb1974@gmail.com>
2025-07-09 19:44:21 +0300

    More WS2812 fun

--- attiny85-ws2812/src/main.cpp
+++ attiny85-ws2812/src/main.cpp
@@ -6,8 +6,8 @@
 
 static CRGB ledStrip[LED_COUNT];
 
-void rainbow(uint8_t hueOffset) {
-  for(uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+void rainbowFlow(uint8_t hueOffset) {
+  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
     uint8_t hue = static_cast<uint8_t>(hueOffset + ledIdx * 10);
     ledStrip[ledIdx] = CHSV(hue, 255, 255);
   }
@@ -15,16 +15,25 @@ void rainbow(uint8_t hueOffset) {
   FastLED.show();
 }
 
-void digitalRGB(uint8_t rgbBits) {
-    uint8_t r = rgbBits & 0x01 ? 255 : 0;
-    uint8_t g = rgbBits & 0x02 ? 255 : 0;
-    uint8_t b = rgbBits & 0x04 ? 255 : 0;
+void rainbowLines(uint8_t hueOffset) {
+  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+    uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
+    ledStrip[ledIdx] = CHSV(hue, 255, 255);
+  }
 
-    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
-      ledStrip[ledIdx] = CRGB(r, g, b);
-    }
+  FastLED.show();
+}
 
-    FastLED.show();
+void rainbowDot(uint8_t hueOffset) {
+  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+    ledStrip[ledIdx] = CHSV(0, 0, 0);
+  }
+
+  uint8_t ledIdx = hueOffset % LED_COUNT;
+  uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
+  ledStrip[ledIdx] = CHSV(hue, 255, 255);
+
+  FastLED.show();
 }
 
 void setup() {
@@ -37,7 +46,13 @@ void setup() {
 
 void loop() {
   for (uint8_t i = 0; i <= 255; i++) {
-    rainbow(i);
+    rainbowFlow(i);
     delay(10);
+
+    // rainbowLines(i);
+    // delay(10);
+
+    // rainbowDot(i);
+    // delay(100);
   }
 }
