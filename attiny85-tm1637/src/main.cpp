b53d80cee33342857aba94541a0e269aa7fde59f
Dan Borlovan <danb1974@gmail.com>
2025-07-10 19:36:38 +0300

    Start configuring clang

--- attiny85-tm1637/src/main.cpp
+++ attiny85-tm1637/src/main.cpp
@@ -8,62 +8,62 @@
 TM1637Display display(CLK, DIO);
 
 void clearDisplay() {
-  uint8_t data[] = {0, 0, 0, 0};
-  display.setSegments(data, 4U, 0U);
+    uint8_t data[] = {0, 0, 0, 0};
+    display.setSegments(data, 4U, 0U);
 }
 
 void segmentTest() {
-  uint8_t data[] = {1, 1, 1, 1};
+    uint8_t data[] = {1, 1, 1, 1};
 
-  for (uint8_t i = 0; i < 6; i++) {
-    for (uint8_t digit = 0; digit < 4; digit++) {
-      data[digit] <<= 1;
+    for (uint8_t i = 0; i < 6; i++) {
+        for (uint8_t digit = 0; digit < 4; digit++) {
+            data[digit] <<= 1;
+        }
+        display.setSegments(data, 4U, 0U);
+
+        delay(300);
     }
-    display.setSegments(data, 4U, 0U);
 
+    clearDisplay();
     delay(300);
-  }
-
-  clearDisplay();
-  delay(300);
 }
 
 void setup() {
-  display.setBrightness(0x03); // 0x0f is maximum
+    display.setBrightness(0x03);  // 0x0f is maximum
 
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, 1);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, 1);
 
-  segmentTest();
+    segmentTest();
 }
 
 void loop() {
-  static uint8_t minutes = 9;
-  static uint8_t seconds = 59;
+    static uint8_t minutes = 9;
+    static uint8_t seconds = 59;
 
-  if (minutes > 0 || seconds > 0) {
-    uint8_t data[] = {
-      display.encodeDigit(minutes / 10),
-      display.encodeDigit(minutes % 10),  
-      display.encodeDigit(seconds / 10),
-      display.encodeDigit(seconds % 10)
-    };
+    if (minutes > 0 || seconds > 0) {
+        uint8_t data[] = {
+            display.encodeDigit(minutes / 10),
+            display.encodeDigit(minutes % 10),
+            display.encodeDigit(seconds / 10),
+            display.encodeDigit(seconds % 10)
+        };
 
-    // Turn on colon by setting bit 7
-    // We toggle it every second
-    data[1] |= (seconds & 0x01) << 7;
+        // Turn on colon by setting bit 7
+        // We toggle it every second
+        data[1] |= (seconds & 0x01) << 7;
 
-    display.setSegments(data, 4U, 0U);
+        display.setSegments(data, 4U, 0U);
 
-    if (seconds > 0) {
-      seconds--;
+        if (seconds > 0) {
+            seconds--;
+        } else {
+            minutes--;
+            seconds = 59;
+        }
     } else {
-      minutes--;
-      seconds = 59;
+        clearDisplay();
     }
-  } else {
-    clearDisplay();
-  }
 
-  delay(1000);
+    delay(1000);
 }
--- attiny85-ws2812/src/main.cpp
+++ attiny85-ws2812/src/main.cpp
@@ -1,109 +1,109 @@
 #define NO_CLOCK_CORRECTION 1
 #include <FastLED.h>
 
-#define LED_PIN     0U
-#define LED_COUNT   16U
-#define INT_LED     9U
+#define LED_PIN 0U
+#define LED_COUNT 16U
+#define INT_LED 9U
 
 static CRGB ledStrip[LED_COUNT];
 
 void rainbowFlow(uint8_t hueOffset) {
-  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
-    uint8_t hue = static_cast<uint8_t>(hueOffset + ledIdx * 10);
-    ledStrip[ledIdx] = CHSV(hue, 255, 255);
-  }
+    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+        uint8_t hue = static_cast<uint8_t>(hueOffset + ledIdx * 10);
+        ledStrip[ledIdx] = CHSV(hue, 255, 255);
+    }
 
-  FastLED.show();
+    FastLED.show();
 }
 
 void rainbowLines(uint8_t hueOffset) {
-  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
-    uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
-    ledStrip[ledIdx] = CHSV(hue, 255, 255);
-  }
+    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+        uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
+        ledStrip[ledIdx] = CHSV(hue, 255, 255);
+    }
 
-  FastLED.show();
+    FastLED.show();
 }
 
 void rainbowDot(uint8_t hueOffset) {
-  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
-    ledStrip[ledIdx] = CHSV(0, 0, 0);
-  }
+    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+        ledStrip[ledIdx] = CHSV(0, 0, 0);
+    }
 
-  uint8_t ledIdx = hueOffset % LED_COUNT;
-  uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
-  ledStrip[ledIdx] = CHSV(hue, 255, 255);
+    uint8_t ledIdx = hueOffset % LED_COUNT;
+    uint8_t hue = static_cast<uint8_t>(hueOffset + (ledIdx / 4) * 20);
+    ledStrip[ledIdx] = CHSV(hue, 255, 255);
 
-  FastLED.show();
+    FastLED.show();
 }
 
 void policeLights(bool red, bool blue) {
-  for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
-    ledStrip[ledIdx] = CHSV(0, 0, 0);
-  }
-
-  if (red) {
-    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT / 2; ledIdx++) {
-     ledStrip[ledIdx] = CRGB(255, 0, 0);
+    for (uint8_t ledIdx = 0; ledIdx < LED_COUNT; ledIdx++) {
+        ledStrip[ledIdx] = CHSV(0, 0, 0);
     }
-  }
 
-  if (blue) {
-    for (uint8_t ledIdx = LED_COUNT / 2; ledIdx < LED_COUNT; ledIdx++) {
-     ledStrip[ledIdx] = CRGB(0, 0, 255);
+    if (red) {
+        for (uint8_t ledIdx = 0; ledIdx < LED_COUNT / 2; ledIdx++) {
+            ledStrip[ledIdx] = CRGB(255, 0, 0);
+        }
     }
-  }
 
-  FastLED.show();
+    if (blue) {
+        for (uint8_t ledIdx = LED_COUNT / 2; ledIdx < LED_COUNT; ledIdx++) {
+            ledStrip[ledIdx] = CRGB(0, 0, 255);
+        }
+    }
+
+    FastLED.show();
 }
 
 void setup() {
-  // declare leds
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(ledStrip, LED_COUNT);
+    // declare leds
+    FastLED.addLeds<WS2812, LED_PIN, GRB>(ledStrip, LED_COUNT);
 
-  // configure leds
-  FastLED.setBrightness(10);
+    // configure leds
+    FastLED.setBrightness(10);
 
-  // signal end of setup
-  pinMode(INT_LED, OUTPUT);
-  digitalWrite(INT_LED, 1);
+    // signal end of setup
+    pinMode(INT_LED, OUTPUT);
+    digitalWrite(INT_LED, 1);
 }
 
 #define MODEL 3
 
 void loop() {
-  switch (MODEL) {
-    case 1:
-      for (uint8_t c = 0; c < 2; c++) {
-        for (uint8_t i = 0; i < 3; i++) {
-          policeLights(c == 0, c == 1);
-          delay(100);
-          policeLights(false, false);
-          delay(100);
-        }
-        delay(200);
-      }
-      break;
+    switch (MODEL) {
+        case 1:
+            for (uint8_t c = 0; c < 2; c++) {
+                for (uint8_t i = 0; i < 3; i++) {
+                    policeLights(c == 0, c == 1);
+                    delay(100);
+                    policeLights(false, false);
+                    delay(100);
+                }
+                delay(200);
+            }
+            break;
 
-    case 2:
-      for (uint8_t i = 0; i <= 255; i++) {
-        rainbowFlow(i);
-        delay(10);
-      }
-      break;
+        case 2:
+            for (uint8_t i = 0; i <= 255; i++) {
+                rainbowFlow(i);
+                delay(10);
+            }
+            break;
 
-    case 3:
-      for (uint8_t i = 0; i <= 255; i++) {
-        rainbowLines(i);
-        delay(10);
-      }
-      break;
+        case 3:
+            for (uint8_t i = 0; i <= 255; i++) {
+                rainbowLines(i);
+                delay(10);
+            }
+            break;
 
-    case 4:
-      for (uint8_t i = 0; i <= 255; i++) {
-        rainbowDot(i);
-        delay(100);
-      }
-      break;
-  }
+        case 4:
+            for (uint8_t i = 0; i <= 255; i++) {
+                rainbowDot(i);
+                delay(100);
+            }
+            break;
+    }
 }
